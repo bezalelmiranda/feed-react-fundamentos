7059a26efa65348c3a651a797833db917b683d69
bezalelmiranda <begmr206@gmail.com>
2023-08-28 17:43:02 -0300

    First commit of react fundamentals

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header";
+
+import './global.css';
+
+import styles from './App.module.css';
+
+export function App() {
+
+  return (
+    <div>
+      <Header/>
+
+     <div className="wrapper"></div>
+    </div>
+  )
+}
+
---
+++ 01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,10 @@
+// props: { author: string, content: string }
+
+export function Post(props){
+    return(
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    );
+}
